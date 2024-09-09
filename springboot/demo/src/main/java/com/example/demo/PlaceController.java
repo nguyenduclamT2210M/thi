73ca3e1e704de2e17bed0169cdb282bd00e04446
Duclam <115681456+nguyenduclamT2210M@users.noreply.github.com>
2024-09-09 10:27:26 +0700

    Add files via upload

---
+++ springboot/demo/src/main/java/com/example/demo/PlaceController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@RequestMapping("/api/places")
+@CrossOrigin(origins = "http://localhost:8081")
+public class PlaceController {
+    private final PlaceService placeService;
+
+    public PlaceController(PlaceService placeService) {
+        this.placeService = placeService;
+    }
+
+    @GetMapping("/getAll")
+    public List<Place> getAllPlaces() {
+        return placeService.getAllPlaces();
+    }
+}
---
+++ springboot/demo/src/main/java/com/example/demo/PlaceService.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PlaceService {
+    private final PlaceRepository placeRepository;
+
+    public PlaceService(PlaceRepository placeRepository) {
+        this.placeRepository = placeRepository;
+    }
+
+    public List<Place> getAllPlaces() {
+        return placeRepository.findAll();
+    }
+}
